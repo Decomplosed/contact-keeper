5e00038b68b0a1996c70ba2edfbf9ad3ebd1e7d3
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-23 00:44:02 +0100

    created a route to update contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -46,7 +46,7 @@ router.post(
         email,
         phone,
         type,
-        user: erq.user.id
+        user: req.user.id
       })
 
       const contact = await newContact.save()
@@ -61,8 +61,34 @@ router.post(
 // @route     PUT api/contacts/:id
 // @desc      Update contact
 // @access    Private
-router.put('/:id', (req, res) => {
-  res.send('Update contact')
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body
+  const contactFields = {}
+
+  if (name) contactFields.name = name
+  if (email) contactFields.email = email
+  if (phone) contactFields.phone = phone
+  if (type) contactFields.type = type
+
+  try {
+    let contact = await Contact.findById(req.params.id)
+
+    if (!contact) return res.status(404).json({ msg: 'Contact not found' })
+
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' })
+    }
+
+    contact = await Contact.findByIdAndUpdate(req.params.id,
+      { $set: contactFields },
+      { new: true })
+
+    res.json(contact)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+
 })
 
 // @route     DELETE api/contacts/:id
