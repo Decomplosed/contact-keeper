e88b7fb1609d8f5d7c39ce14932d601607d258fd
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-23 23:59:11 +0100

    solved type

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,4 +1,4 @@
-import React, { useRedeucer } from 'react'
+import React, { useReducer } from 'react'
 import uuid from 'uuid'
 import ContactContext from './contactContext'
 import contactReducer from './contactReducer'
