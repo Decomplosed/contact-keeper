518805328a3e7da937a0bb3b0fa887643ea6225f
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-23 23:50:09 +0100

    brought all the contexts to contact state file

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,3 +2,13 @@ import React, { useRedeucer } from 'react'
 import uuid from 'uuid'
 import ContactContext from './contactContext'
 import contactReducer from './contactReducer'
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types'
+
