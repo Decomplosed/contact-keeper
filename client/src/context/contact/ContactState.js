ffd739e198fe32524bc5701b197eff6af2bb0c4c
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-24 16:13:31 +0100

    connected contact form component with home component

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -43,6 +43,11 @@ const ContactState = props => {
 
   // Add contact
 
+  const addContact = contact => {
+    contact.id = uuid.v4()
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  }
+
   // Delete Contact
 
   // Set current contact
@@ -58,7 +63,8 @@ const ContactState = props => {
   return (
     <ContactContext.Provider
       value={{
-        contacts: state.contacts
+        contacts: state.contacts,
+        addContact,
       }}>
       {props.children}
     </ContactContext.Provider>
