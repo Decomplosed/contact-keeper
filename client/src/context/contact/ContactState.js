eba8adaa725230e122ec1059a9c0b4d246eebcf7
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-25 23:13:21 +0100

    added the deleteContact method into ContactState

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -50,6 +50,10 @@ const ContactState = props => {
 
   // Delete Contact
 
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id })
+  }
+
   // Set current contact
 
   // Clear current contact
@@ -65,6 +69,7 @@ const ContactState = props => {
       value={{
         contacts: state.contacts,
         addContact,
+        deleteContact
       }}>
       {props.children}
     </ContactContext.Provider>
