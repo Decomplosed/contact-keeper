ffbbbe9742b2242714cdaa3002f2349f4ad631c8
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-26 21:57:07 +0100

    small destructuring and imported useEffect for filtered value of the input

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext, useRef, useEffect } from 'react'
 import ContactContext from '../../context/contact/contactContext'
 
 
@@ -6,11 +6,19 @@ const ContactFilter = () => {
   const contactContext = useContext(ContactContext)
   const text = useRef('')
 
+  const { filterContacts, clearFilter, filtered } = contactContext
+
+  useEffect(() => {
+    if (filtered === '') {
+      text.current.value = ''
+    }
+  })
+
   const onChange = e => {
     if (text.current.value !== '') {
-      contactContext.filterContacts(e.target.value)
+      filterContacts(e.target.value)
     } else {
-      contactContext.clearFilter()
+      clearFilter()
     }
   }
 
