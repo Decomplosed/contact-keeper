a25e919f039ef9f0a8ef152a9cd029ce436eec73
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-26 23:12:58 +0100

    finished creating the Login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,37 +2,26 @@ import React, { useState } from 'react'
 
 const Login = () => {
   const [user, setUser] = useState({
-    name: '',
     email: '',
-    password: '',
-    password2: ''
+    password: ''
   })
 
-  const { name, email, password, password2 } = user
+  const { email, password } = user
 
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value })
 
   const onSubmit = e => {
     e.preventDefault()
 
-    console.log('Register submit')
+    console.log('Login submit')
   }
 
   return (
     <div className='form-container'>
       <h1>
-        Account <span className='text-primary'>Register</span>
+        Account <span className='text-primary'>Login</span>
       </h1>
       <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label htmlFor='name'>Name</label>
-          <input
-            type='text'
-            name='name'
-            value={name}
-            onChange={onChange}
-          />
-        </div>
         <div className='form-group'>
           <label htmlFor='email'>Email Address</label>
           <input
@@ -51,16 +40,7 @@ const Login = () => {
             onChange={onChange}
           />
         </div>
-        <div className='form-group'>
-          <label htmlFor='password2'>Confirm Password</label>
-          <input
-            type='password'
-            name='password2'
-            value={password2}
-            onChange={onChange}
-          />
-        </div>
-        <input type='submit' value='Register' className='btn btn-primary btn-block' />
+        <input type='submit' value='Login' className='btn btn-primary btn-block' />
       </form>
     </div>
   )
