f0ea7a12337b0dc1d00ebcdb609772fb97f1aaba
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-26 22:53:53 +0100

    created initial structure for AuthState

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from 'react'
+import AuthContext from './authContext'
+import authReducer from './authReducer'
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from '../types'
+
+const AuthState = props => {
+  const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    error: null
+  }
+
+  const [state, dispatch] = useReducer(authReducer, initialState)
+
+
+
+  return (
+    <ContactContext.Provider
+      value={{
+
+      }}>
+      {props.children}
+    </ContactContext.Provider>
+  )
+}
+
+export default AuthState
