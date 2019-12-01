dd2e5b746cbd26e2ba64e43be854879fa0a03e8e
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-01 19:51:06 +0100

    added case of GET_CONTACTS

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -6,15 +6,23 @@ import {
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER,
-  CONTACT_ERROR
+  CONTACT_ERROR,
+  GET_CONTACTS
 } from '../types'
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_CONTACTS
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false
+      }
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload]
+        contacts: [...state.contacts, action.payload],
+        loading: false
       }
     case UPDATE_CONTACT:
       return {
