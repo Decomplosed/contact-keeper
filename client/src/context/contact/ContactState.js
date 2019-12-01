3b487cfa61f708e734e82137ece00f31cd9e813a
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-01 21:35:58 +0100

    added updaate functionality for a contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -72,11 +72,36 @@ const ContactState = props => {
 
   const deleteContact = async id => {
     try {
-      const res = await axios.delete(`/api/contacts/${id}`)
+      await axios.delete(`/api/contacts/${id}`)
 
       dispatch({
         type: DELETE_CONTACT,
         payload: id
+      })
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      })
+    }
+  }
+
+  // Update Contact
+
+  const updateContact = async contact => {
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}`, contact, config)
+
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
       })
     } catch (err) {
       dispatch({
@@ -100,12 +125,6 @@ const ContactState = props => {
 
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT })
-  }
-
-  // Update Contact
-
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact })
   }
 
   // Filter Contacts
