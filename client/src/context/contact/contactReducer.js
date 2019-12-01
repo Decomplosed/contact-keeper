2ffe9b9fc1432f773a6c87a93e17b959d817c8b0
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-01 19:52:08 +0100

    added loading: false for all CRUD cases

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -27,12 +27,14 @@ export default (state, action) => {
     case UPDATE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)
+        contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact),
+        loading: false
       }
     case DELETE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== action.payload)
+        contacts: state.contacts.filter(contact => contact.id !== action.payload),
+        loading: false
       }
     case SET_CURRENT:
       return {
