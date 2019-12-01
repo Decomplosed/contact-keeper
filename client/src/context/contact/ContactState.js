760af205bc906292c1948b06775f9c62a1fad592
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-01 19:49:47 +0100

    created a function to get contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -25,6 +25,25 @@ const ContactState = props => {
 
   const [state, dispatch] = useReducer(contactReducer, initialState)
 
+  // Get Contacts
+
+  const getContacts = async () => {
+
+    try {
+      const res = await axios.get('/api/contacts')
+
+      dispatch({
+        type: GET_CONTACTS,
+        payload: res.data
+      })
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      })
+    }
+  }
+
   // Add contact
 
   const addContact = async contact => {
